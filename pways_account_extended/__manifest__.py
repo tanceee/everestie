7b0bb0b16ed779a94a87015fe8724a1295a7e0c5
masterital <info@master.al>
2022-12-20 09:08:25 +0100

    update pways_account_extended

--- pways_account_extended/__manifest__.py
+++ pways_account_extended/__manifest__.py
@@ -1,20 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Pways invoice report",
-    'summary': "Pways invoice report",
-    'description': "Pways invoice report",
+    'name': "LOT pos invoice report",
+    'summary': "LOT pos invoice report",
+    'description': "LOT pos invoice report",
     'author' : 'Preciseways',
     'website': "http://www.preciseways.com",
     'category': 'account',
     'version': '15.0.0',
-    'depends': ['account', 'stock_account', 'sale_stock'],
-    'data': [
-        'views/invoice_report.xml',
-    ],
-    
+    'depends': ['point_of_sale','stock_account' ,'product_expiry'],
+    'data': ['views/invoice_report.xml'],
     'installable': True,
     'application': True,
-    'price': 15.0,
     'currency': 'EUR',
     'license': 'OPL-1',
 
