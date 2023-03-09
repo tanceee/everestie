71e0ba59aacdfee924d845468d7d696465b789b7
masterital <info@master.al>
2023-03-09 14:43:52 +0100

    delete

--- pways_invoice_report/models/res_partner.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-class ResPartner(models.Model):
-    _inherit = "res.partner"
-
-    license_plate_no = fields.Char(string="License Plate No")
