80e552d7e6817a4a8c83dd5a148fa585ba1056d5
masterital <info@master.al>
2023-03-14 16:55:48 +0100

    lot_expiry_ext

---
+++ lot_expiry_ext/__manifest__.py
@@ -0,0 +1,13 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Lot Expiry Extended',
+    'version': '15.0.1.0.0',
+    'summary': """Lot Expiry date in outgoing picking""",
+    'description': """Lot Expiry date in outgoing picking""",
+    'depends': ['product_expiry'],
+    'data': ["views/product_views.xml",],
+    'auto_install': False,
+    'installable': True,
+    'application': True,
+}
