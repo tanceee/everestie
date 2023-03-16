e5d50e3594ebe8eac21f725b9e88ac101b882236
masterital <info@master.al>
2023-03-16 16:40:41 +0100

    update

--- lot_expiry_ext/models/lot.py
+++ lot_expiry_ext/models/lot.py
@@ -9,19 +9,6 @@ from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 class ProductionLot(models.Model):
 	_inherit = "stock.production.lot"
 
-	# @api.model
-	# def search_read(self, domain=None, fields=None, offset=0, limit=None, order=None):
-	# 	fields.append('expiration_date')
-	# 	result = super().search_read(domain=domain, fields=fields, offset=offset, limit=limit, order=order)
-	# 	for lot in result:
-	# 		lot['name'] = '%s-%s' %(lot.get('name'), lot.get('expiration_date'))
-	# 	return result
-
-	# def name_get(self):
-	# 	self.browse(self.ids).read(['name', 'expiration_date'])
-	# 	return [(lot.id, '%s%s' % (lot.expiration_date and '[%s] ' % lot.expiration_date or '', lot.name))
-    #             for lot in self]
-
 	def convert_TZ_UTC(self, TZ_datetime):
 		user_tz = self.env.user.tz or pytz.utc
 		local = pytz.timezone(user_tz)
@@ -29,12 +16,18 @@ class ProductionLot(models.Model):
 			time = datetime.strftime(pytz.utc.localize(datetime.strptime(TZ_datetime, DEFAULT_SERVER_DATETIME_FORMAT)).astimezone(local),"%m/%d/%Y %H:%M:%S")
 			return time
 
-	def name_get(self):
-		res = []
-		self.browse(self.ids).read(['name', 'expiration_date'])
-		for rec in self:
-			expiration_date = False
-			if rec.expiration_date:
-				expiration_date = self.convert_TZ_UTC(fields.Datetime.to_string(rec.expiration_date))
-			res.append((rec.id, '%s - %s' % (rec.name, expiration_date) if rec.expiration_date else rec.name))
-		return res
+
+	# def name_get(self):
+	# 	self.browse(self.ids).read(['name', 'expiration_date'])
+	# 	return [(lot.id, '%s%s' % (lot.expiration_date and '[%s] ' % lot.expiration_date or '', lot.name))
+    #             for lot in self]
+
+	# def name_get(self):
+	# 	res = []
+	# 	self.browse(self.ids).read(['name', 'expiration_date'])
+	# 	for rec in self:
+	# 		expiration_date = False
+	# 		if rec.expiration_date:
+	# 			expiration_date = self.convert_TZ_UTC(fields.Datetime.to_string(rec.expiration_date))
+	# 		res.append((rec.id, '%s - %s' % (rec.name, expiration_date) if rec.expiration_date else rec.name))
+	# 	return res
