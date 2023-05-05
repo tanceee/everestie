d203ececa76819fa48a8e6fc59f88848d44024ea
masterital <info@master.al>
2023-05-05 16:10:30 +0200

    pways_sale_lot_expiry

---
+++ pways_sale_lot_expiry/models/lot.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+import pytz
+from odoo import api, fields, models
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+
+
+class SaleOrderLine(models.Model):
+	_inherit = "sale.order.line"
+
+
+	lot_id = fields.Many2one("stock.production.lot", "Lot", copy=False)
+	expiration_date = fields.Datetime(copy=False)
+
+	@api.onchange("product_id")
+	def product_id_change(self):
+		res = super().product_id_change()
+		self.lot_id = False
+		return res
+
+	@api.onchange("lot_id")
+	def lot_id_onchange(self):
+		if self.lot_id:
+			self.expiration_date = self.lot_id.expiration_date
+
+	@api.onchange("product_id")
+	def _onchange_product_id_set_lot_domain(self):
+		available_lot_ids = []
+		if self.order_id.warehouse_id and self.product_id:
+			location = self.order_id.warehouse_id.lot_stock_id
+			quants = self.env["stock.quant"].read_group(
+				[
+					("product_id", "=", self.product_id.id),
+					("location_id", "child_of", location.id),
+					("quantity", ">", 0),
+					("lot_id", "!=", False),
+				],
+				["lot_id"],
+				"lot_id",
+			)
+			available_lot_ids = [quant["lot_id"][0] for quant in quants]
+		self.lot_id = False
+		return {"domain": {"lot_id": [("id", "in", available_lot_ids)]}}
+
+class StockMove(models.Model):
+	_inherit = "stock.move"
+
+	def _update_reserved_quantity(self,need,available_quantity,location_id,lot_id=None,package_id=None,owner_id=None,strict=True,):
+		if self.sale_line_id.lot_id:
+			lot_id = self.sale_line_id.lot_id
+		return super()._update_reserved_quantity(need,available_quantity,location_id,lot_id=lot_id,package_id=package_id,owner_id=owner_id,strict=strict)
+
+	def _prepare_move_line_vals(self, quantity=None, reserved_quant=None):
+		vals = super()._prepare_move_line_vals(quantity=quantity, reserved_quant=reserved_quant)
+		if reserved_quant and self.sale_line_id.lot_id and self.sale_line_id.expiration_date:
+			vals.update({"lot_id" : self.sale_line_id.lot_id.id, 'expiration_date' : self.sale_line_id.expiration_date})
+		return vals
